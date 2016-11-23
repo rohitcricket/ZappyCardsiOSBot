fd579af1e56c4e8b825809edbcb7817d84738da1
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-11-22 20:54:50 -0800

    first commit

---
+++ CustomObject.m
@@ -0,0 +1,10 @@
+//
+//  CustomObject.m
+//  ZappyCardsBot
+//
+//  Created by ROHIT GUPTA on 11/21/16.
+//  Copyright © 2016 ROHIT GUPTA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Smooch/Smooch.h>
---
+++ ZappyCardsBot/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ZappyCardsBot
+//
+//  Created by ROHIT GUPTA on 11/21/16.
+//  Copyright © 2016 ROHIT GUPTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Smooch.show()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
